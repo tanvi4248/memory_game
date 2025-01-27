a3ce360b9ef94b2cf0c679f0c328290185273dfe
tanvi4248 <tanvi@GWTN141-10>
2025-01-27 17:33:31 -0600

    add flipped card condition

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./SingleCard.css";
-export default function SingleCard({ card, handleChoice }) {
+export default function SingleCard({ card, handleChoice, flipped }) {
   const handleClick = () => {
     handleChoice(card);
   };
   return (
     <div className="card">
-      <div>
+      <div className={flipped ? "flipped" : ""}>
         <img className="front-img" src={card.src} alt="fimage"></img>
         <div className="back-bg" onClick={handleClick}></div>
       </div>
